7ac05275c766186a8ac82ba926c27b3a6a9bdef5
Batuhan Taşkaya <batuhanosmantaskaya@gmail.com>
2018-12-30 12:56:10 +0300

    versionbumptest

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="darwcss",
-    version="0.2.1",
+    version="0.2.2",
     author="BTaskaya",
     author_email="batuhanosmantaskaya@gmail.com",
     packages=find_packages(),
