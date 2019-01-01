0088900ca4736eeee7df580557a5d09d153bf59f
Batuhan Taşkaya <batuhanosmantaskaya@gmail.com>
2019-01-01 17:09:12 +0300

    fix flake8 command

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="darwcss",
-    version="0.2.3",
+    version="0.3",
     author="BTaskaya",
     author_email="batuhanosmantaskaya@gmail.com",
     packages=find_packages(),
